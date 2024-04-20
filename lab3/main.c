096850d726394aad0de53185294a1261e6b0c41e
422101abhi <9570princebeta@gmail.com>
2024-04-20 21:27:36 +0530

    assigment3

---
+++ lab3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lib.h"
+
+int main()
+{
+
+int a=5;
+int b=6;
+
+printf("the sum of number is %d ",sum(a,b));
+printf("the differnce of number is %d ",diff(a,b));
+
+return 0;
+}
